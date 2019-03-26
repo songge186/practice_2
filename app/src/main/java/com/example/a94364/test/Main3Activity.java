359a09a5da9c8477a7883ad6f28f296c53fe1988
songge186 <943642543@qq.com>
2019-03-26 22:59:19 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a94364/test/Main3Activity.java
@@ -0,0 +1,13 @@
+package com.example.a94364.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+}
